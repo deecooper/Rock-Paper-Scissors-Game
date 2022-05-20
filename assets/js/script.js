522aad0dff8d66ca03f5bf5d60a7cfc815ab85eb
deecooper <deecoopermedia@gmail.com>
2022-05-20 03:03:09 +0000

    Add a display function for images to appear when button pressed

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,52 @@ let computerChoiceDisplay = document.getElementById("computer-choice");
 let playerChoiceDisplay = document.getElementById("player-choice");
 let playerScoreDisplay = document.getElementById("player-score");
 let computerScoreDisplay = document.getElementById("computer-score");
+const rockRef = document.querySelector('#rock');
+const scissorsRef = document.querySelector('#scissors');
+const paperRef = document.querySelector('#paper');
+let computerImage = document.getElementById('computer-image');
+
+let resultDisplay = document.getElementById("result")
 let buttonChoices = document.querySelectorAll("button");
-
-
+let scoreDisplay = document.getElementById("score")
 let playerChoice;
 let computerChoice;
+let computerScore = 0;
+let playerScore = 0;
+let result;
+let gameCompleted = false;
 
-//vent listener for player
+
+const controlOptions = {
+    rock: "./assets/images/rock.png",
+    paper: "./assets/images/paper.png",
+    scissors: "./assets/images/scissors.png",
+   
+};
+
+rockRef.addEventListener('click', () => {
+    displayUserChoice('rock');});
+
+scissorsRef.addEventListener('click', () => {
+    displayUserChoice('scissors');});
+
+paperRef.addEventListener('click', () => {
+    displayUserChoice('paper');});
+
+
+    function displayUserChoice(control) { 
+        document.getElementById("player-image").src = controlOptions[control];
+    
+        let userContest = document.getElementById("player-image");
+          userContest.style.visibility = "visible" ;
+        }
+
 
 buttonChoices.forEach(buttonChoice => buttonChoice.addEventListener("click", (e) => {
     playerChoice = e.target.id;
     playerChoiceDisplay.innerHTML = playerChoice;
+    
+  
 
 
     computerSelection();
@@ -22,8 +57,6 @@ buttonChoices.forEach(buttonChoice => buttonChoice.addEventListener("click", (e)
 
 }));
 
-
-//Computer Selection function
 
 function computerSelection() {
     let randomNumber = Math.floor(Math.random() * 3);
@@ -37,10 +70,14 @@ function computerSelection() {
         computerChoice = "scissors";
 
     }
-
     computerOptions = ["rock", "paper", "scissors"]
 
     computerChoiceDisplay.innerHTML = computerChoice;
     let computerChoose = computerOptions[randomNumber];
+    console.log(computerChoose)
+    computerImage.src = `./assets/images/${computerChoiceDisplay.innerHTML}.png`; 
+    computerImage.style.visibility = "visible" ;  
+
 
 }
+
