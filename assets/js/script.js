991c70076f107fb953cd2cbb89f6ef224dfea6e9
deecooper <deecoopermedia@gmail.com>
2022-05-20 02:54:32 +0000

    Add in function for computer selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@ let buttonChoices = document.querySelectorAll("button");
 let playerChoice;
 let computerChoice;
 
+//vent listener for player
 
 buttonChoices.forEach(buttonChoice => buttonChoice.addEventListener("click", (e) => {
     playerChoice = e.target.id;
@@ -20,3 +21,26 @@ buttonChoices.forEach(buttonChoice => buttonChoice.addEventListener("click", (e)
     getScore();
 
 }));
+
+
+//Computer Selection function
+
+function computerSelection() {
+    let randomNumber = Math.floor(Math.random() * 3);
+
+    if (randomNumber === 0) {
+        computerChoice = "rock";
+    } else if (randomNumber === 1) {
+        computerChoice = "paper";
+
+    } else if (randomNumber === 2) {
+        computerChoice = "scissors";
+
+    }
+
+    computerOptions = ["rock", "paper", "scissors"]
+
+    computerChoiceDisplay.innerHTML = computerChoice;
+    let computerChoose = computerOptions[randomNumber];
+
+}
